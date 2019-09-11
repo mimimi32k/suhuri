dd1ab8988ae41b42427a77932c3df0eb3aeb3e29
mimimi32k <comame.mizuki@gmail.com>
2019-09-11 20:14:23 +0900

    add_image

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root 'messages#index'
   resources :messages
   resources :users, only: [:edit, :update]
+  resources :schools, only: [:index]
+  resources :calendars, only: [:index]
+  resources :reservations, only: [:index]
+  resources :accesses, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
